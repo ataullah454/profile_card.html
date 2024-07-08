52cb22e85ece979ef316d9d069793ecea81bcd5d
md ataullah <121761337+ataullah454@users.noreply.github.com>
2024-07-08 18:20:22 +0600

    Add files via upload

---
+++ celcious_to_fahrenheit.php/index.php
@@ -0,0 +1,5 @@
+<?php
+$celsius =  32;
+$fahrenheit = ($celsius * 9/5) +32;
+printf("temperature in fahrenhit: %.2f",$fahrenheit);
+?>
---
+++ largest_number.php/index.php
@@ -0,0 +1,16 @@
+<?php //veriabal num
+$num1 = 4;
+$num2 = 5;
+$num3 = 6;
+
+//condition check
+if($num1 <5){
+    echo "the number $num1 is less then $num2";
+}elseif($num2 == $num3){
+    echo "the number $num2 is equal to $num3";
+}elseif($num1 > $num2){
+    echo "the number $num1 is grather then $num2";
+}else{
+    echo "zero";
+}
+?>
